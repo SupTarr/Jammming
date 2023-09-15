cc7890f1cd02b01d90f16b3b5877ae74e731858b
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-09-15 23:41:40 +0700

    feat: style login section

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -4,6 +4,10 @@ import "./App.css";
 import Navbar from "../Navbar/Navbar.jsx";
 import DashBoard from "../Dashboard/DashBoard";
 
+const clientId = import.meta.env.VITE_API_KEY;
+const redirectUri = import.meta.env.VITE_FRONTEND_URL;
+const spotifyUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-modify-public`;
+
 const codeFromURL = new URLSearchParams(window.location.search).get("code");
 
 const App = () => {
@@ -17,7 +21,14 @@ const App = () => {
   return (
     <div>
       <Navbar code={code} onLogout={() => setCode("")} />
-      <div className="App">{code && <DashBoard code={code} />}</div>
+      <div className="App">
+        {!code && (
+          <a className="login-button" href={spotifyUrl}>
+            Login with Spotify
+          </a>
+        )}
+        {code && <DashBoard code={code} />}
+      </div>
     </div>
   );
 };
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import "./Navbar.css";
 
-const clientId = import.meta.env.VITE_API_KEY;
-const redirectUri = import.meta.env.VITE_FRONTEND_URL;
-const spotifyUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-modify-public`;
-
 const Navbar = ({ code, onLogout }) => {
   return (
     <nav>
@@ -12,19 +8,15 @@ const Navbar = ({ code, onLogout }) => {
         Ja<span className="highlight">mmm</span>ing
       </h1>
 
-      {code ? (
+      {code && (
         <a
-          className="login-button"
+          className="logout-button"
           onClick={(e) => {
             e.preventDefault();
             onLogout();
           }}
         >
           Logout
-        </a>
-      ) : (
-        <a className="login-button" href={spotifyUrl}>
-          Login
         </a>
       )}
     </nav>
