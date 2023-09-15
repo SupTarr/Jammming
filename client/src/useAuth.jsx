93c05b21b5bab63f5f2b413517b996740191b9a7
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-09-16 00:10:37 +0700

    feat: use environment variable

--- client/src/useAuth.jsx
+++ client/src/useAuth.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+const backend = import.meta.env.VITE_BACKEND_URL;
+
 export default function useAuth(code) {
   const [accessToken, setAccessToken] = useState();
   const [refreshToken, setRefreshToken] = useState();
@@ -8,7 +10,7 @@ export default function useAuth(code) {
 
   useEffect(() => {
     axios
-      .post("http://localhost:3001/login", {
+      .post(`${backend}login`, {
         code,
       })
       .then((res) => {
@@ -24,22 +26,19 @@ export default function useAuth(code) {
 
   useEffect(() => {
     if (!refreshToken || !expiresIn) return;
-    const interval = setInterval(
-      () => {
-        axios
-          .post("http://localhost:3001/refresh", {
-            refreshToken,
-          })
-          .then((res) => {
-            setAccessToken(res.data.accessToken);
-            setExpiresIn(res.data.expiresIn);
-          })
-          .catch(() => {
-            window.location = "/";
-          });
-      },
-      (expiresIn - 60) * 1000,
-    );
+    const interval = setInterval(() => {
+      axios
+        .post(`${backend}refresh`, {
+          refreshToken,
+        })
+        .then((res) => {
+          setAccessToken(res.data.accessToken);
+          setExpiresIn(res.data.expiresIn);
+        })
+        .catch(() => {
+          window.location = "/";
+        });
+    }, (expiresIn - 60) * 1000);
 
     return () => clearInterval(interval);
   }, [refreshToken, expiresIn]);
