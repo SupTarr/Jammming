3ba422b8e65ec28abed5fabf4b12f9b5179f5960
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-09-17 00:02:38 +0700

    feat: add player full screen mode

--- client/src/components/Player/Player.jsx
+++ client/src/components/Player/Player.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 import SpotifyPlayer from "react-spotify-player";
 
-function Player({ accessToken, trackUri }) {
+function Player({ track }) {
+  const [isFullScreen, setIsFullScreen] = React.useState(false);
+
   return (
-    <div style={{ position: "fixed", width: "100vw", bottom: "0px" }}>
-      <SpotifyPlayer
-        uri="spotify:album:1TIUsv8qmYLpBEhvmBmyBk"
-        view="list"
-        theme="black"
-      />
-    </div>
+    <>
+      {isFullScreen ? (
+        <div style={{ position: "fixed", height: "100vh", top: "25%" }}>
+          <SpotifyPlayer uri={track} view="list" theme="black" />
+        </div>
+      ) : (
+        <div
+          style={{
+            position: "fixed",
+            width: "100vw",
+            bottom: "0px",
+            height: "80px",
+          }}
+        >
+          <SpotifyPlayer
+            uri={track}
+            size={{ width: "100%" }}
+            view="list"
+            theme="black"
+          />
+        </div>
+      )}
+    </>
   );
 }
 
