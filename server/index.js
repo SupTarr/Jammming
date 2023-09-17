72fd483ee4acfe4d1348e9336199d44af784f880
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-09-17 15:32:38 +0700

    fix: remove lyric finder

--- server/index.js
+++ server/index.js
@@ -126,11 +126,4 @@ app.post("/save", async (req, res) => {
     });
 });
 
-app.get("/lyrics", async (req, res) => {
-  const lyrics =
-    (await lyricsFinder(req.query.artist, req.query.track)) ||
-    "No Lyrics Found";
-  res.json({ lyrics });
-});
-
 app.listen(3001);
