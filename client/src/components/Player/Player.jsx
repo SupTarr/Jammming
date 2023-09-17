02585f49fef7687c06efb45836f2f62c1cc3b6bc
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-09-17 11:23:45 +0700

    feat: change new player style

--- client/src/components/Player/Player.jsx
+++ client/src/components/Player/Player.jsx
@@ -12,7 +12,7 @@ function Player({ track }) {
       <iframe
         src={getUrlFromTrackUri(track)}
         width="100%"
-        height="80"
+        height="152"
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy"
       />
