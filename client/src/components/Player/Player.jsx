bac4a29fc0b0074521a4923bb710a437850a9c41
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-09-17 09:54:47 +0700

    fix: remove fullscreen player

--- client/src/components/Player/Player.jsx
+++ client/src/components/Player/Player.jsx
@@ -3,25 +3,15 @@ import "./Player.css";
 import SpotifyPlayer from "react-spotify-player";
 
 function Player({ track }) {
-  const [isFullScreen, setIsFullScreen] = React.useState(false);
-
   return (
-    <>
-      {isFullScreen ? (
-        <div className="Player-fullscreen">
-          <SpotifyPlayer uri={track} view="list" theme="black" />
-        </div>
-      ) : (
-        <div className="Player-fixed-bottom">
-          <SpotifyPlayer
-            uri={track}
-            size={{ width: "100%" }}
-            view="list"
-            theme="black"
-          />
-        </div>
-      )}
-    </>
+    <div className="Player">
+      <SpotifyPlayer
+        uri={track}
+        size={{ width: "100%" }}
+        view="list"
+        theme="black"
+      />
+    </div>
   );
 }
 
